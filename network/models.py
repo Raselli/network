3c53b4f257a8cb44d631fbc709e0f74f13bf6d1c
Raselli <82221324+Raselli@users.noreply.github.com>
2022-05-17 17:11:23 +0200

    added models to models.py

--- network/models.py
+++ network/models.py
@@ -1,22 +1,26 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
 class User(AbstractUser):
     pass
 
-# class Post
-    # Foreign key: User or Userprofile
-    # fields: username (inh.: foreign key), content (text), likes (int, default=0), timestamp (daytime)
+class Post(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
+    likes = models.IntegerField(default=0)
+    posted = models.DateTimeField(auto_now_add=True)
+     
+    def __str__(self):
+        return f"{self.username} posted on {self.posted}: \"{self.content}\". [{self.likes} like(s)]" 
 
-# class user profile
-    # Foreign key: User
-    # fields: username (inh.: foreign key), count: followers (int), count: following (int)
+class Profile(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    followers = models.IntegerField(default=0)
+    following = ArrayField(models.IntegerField(unique=True))
+    liked = ArrayField(models.IntegerField(unique=True))   
     # ? either: query for assosication list 'followers' & 'following' or save int on profile
-        # for higher numbers: better, for this excercise: prob. overkill
-    
-# class like
-    # foreign keys: post, users profile
-    
-# class follow
-    # foreign keys: user profile (other), users profile (auth. user) OR user instead of u.profile
+
+    def __str__(self):
+        return f"{self.user.username} has {self.followers} follower(s) and follows {self.following}" 
