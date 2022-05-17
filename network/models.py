7d6a7c1f1a3a84acafa054381d1cd5a4885eb7bc
Raselli <82221324+Raselli@users.noreply.github.com>
2022-05-17 11:48:01 +0200

    pseudocode

--- network/models.py
+++ network/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+# class Post
+    # Foreign key: User or Userprofile
+    # fields: username (inh.: foreign key), content (text), likes (int, default=0), timestamp (daytime)
+
+# class user profile
+    # Foreign key: User
+    # fields: username (inh.: foreign key), count: followers (int), count: following (int)
+    # ? either: query for assosication list 'followers' & 'following' or save int on profile
+        # for higher numbers: better, for this excercise: prob. overkill
+    
+# class like
+    # foreign keys: post, users profile
+    
+# class follow
+    # foreign keys: user profile (other), users profile (auth. user) OR user instead of u.profile
--- network/views.py
+++ network/views.py
@@ -61,3 +61,40 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+# add ModelForm create_post
+
+# def create_post
+    # let user create new post
+
+# def posts
+    # load all posts from all users
+    # descending based on timestamp: new -> old
+    # post includes: username of creator, content, timestamp (mm/dd/yy, hh:mm), likes
+    # Pagination:
+        # render only 10 postsat once
+        # next button for next 10 posts
+        # back button for prev 10 posts
+    # like/unlike button: JS, asynch., update like-count (no re-render)
+    
+# def user_profile
+    # display: followers & following
+    # display user's posts (reverse chronological order: newest -> oldest)
+    # if visitor != user: follow/unfollow button
+    
+# def following
+    # load all (recent?) posts of users the auth. user follows
+    # login req.
+    # ? merge with 'def posts' under same rendering with switchstatement/dict of different db queries ?
+    # Pagination:
+        # render only 10 postsat once
+        # next button for next 10 posts
+        # back button for prev 10 posts
+
+# add ModelForm edit_post
+  
+# def edit_post
+    # load textarea with posts content inside
+    # save edited content with save button
+        # save with JS: no re-render of page
+    # check security: token, manipulation, get/post: check user_id
