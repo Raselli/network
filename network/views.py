ee3873caabb8402599547448f11373a8976f3047
Raselli <82221324+Raselli@users.noreply.github.com>
2022-05-18 17:58:27 +0200

    added index, create_post, profile to views.py

--- network/views.py
+++ network/views.py
@@ -1,14 +1,92 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from django.core.exceptions import ValidationError
 
+from .models import User, Profile, Post
+
+# Form: create post
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["content"]
+        
+        widgets = {
+            "content": forms.Textarea(attrs={"rows":3, "cols":10})
+        }
+        
+        labels = {
+            "content": "New Post"
+        }
 
 def index(request):
-    return render(request, "network/index.html")
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid:
+            form = form.save(commit=False)
+            form.user_id = request.user.id
+            form.save()
+            return HttpResponseRedirect("/")
+        else:
+# TODO: VALIDATION ERROR HANDLING
+            return HttpResponseRedirect("/")            
+    else:
+        return render(request, "network/index.html" , {
+            "form": PostForm(),
+            "posts": Post.objects.select_related().order_by('-posted')
+        })
+# TODO:
+    # Pagination:
+        # render only 10 postsat once
+        # next button for next 10 posts
+        # back button for prev 10 posts
+    # like/unlike button: JS, asynch., update like-count (no re-render)
+        
+    # def user_profile
+        # display: followers & following
+        # display user's posts (reverse chronological order: newest -> oldest)
+        # if visitor != user: follow/unfollow button
+
+def user_profile(request, username):
+
+    # Query profile
+    try: 
+        user = User.objects.get(username=username)
+    except:
+        raise Http404("Profile does not exist.")
+    
+    return render(request, "network/index.html" , {
+        "profile": Profile.objects.prefetch_related().get(user=user.id),
+        "form": PostForm(),
+        "posts": Post.objects.filter(user=user.id).select_related().order_by('-posted')
+    })
+
+# profile on seperate page or on same?
+
+# def following
+    # load all (recent?) posts of users the auth. user follows
+    # login req.
+    # ? merge with 'def posts' under same rendering with switchstatement/dict of different db queries ?
+    # Pagination:
+        # render only 10 postsat once
+        # next button for next 10 posts
+        # back button for prev 10 posts
+
+# add ModelForm edit_post
+  
+# def edit_post
+    # load textarea with posts content inside
+    # save edited content with save button
+        # save with JS: no re-render of page
+    # check security: token, manipulation, get/post: check user_id
+
+
+
+
 
 
 def login_view(request):
@@ -53,6 +131,10 @@ def register(request):
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
+            
+# create profile
+            profile = Profile.objects.create(user=user)
+            profile.save()
         except IntegrityError:
             return render(request, "network/register.html", {
                 "message": "Username already taken."
@@ -62,39 +144,3 @@ def register(request):
     else:
         return render(request, "network/register.html")
 
-# add ModelForm create_post
-
-# def create_post
-    # let user create new post
-
-# def posts
-    # load all posts from all users
-    # descending based on timestamp: new -> old
-    # post includes: username of creator, content, timestamp (mm/dd/yy, hh:mm), likes
-    # Pagination:
-        # render only 10 postsat once
-        # next button for next 10 posts
-        # back button for prev 10 posts
-    # like/unlike button: JS, asynch., update like-count (no re-render)
-    
-# def user_profile
-    # display: followers & following
-    # display user's posts (reverse chronological order: newest -> oldest)
-    # if visitor != user: follow/unfollow button
-    
-# def following
-    # load all (recent?) posts of users the auth. user follows
-    # login req.
-    # ? merge with 'def posts' under same rendering with switchstatement/dict of different db queries ?
-    # Pagination:
-        # render only 10 postsat once
-        # next button for next 10 posts
-        # back button for prev 10 posts
-
-# add ModelForm edit_post
-  
-# def edit_post
-    # load textarea with posts content inside
-    # save edited content with save button
-        # save with JS: no re-render of page
-    # check security: token, manipulation, get/post: check user_id
