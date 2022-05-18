be98afe0b296014f8b64d167aaa2475e31a8460f
Raselli <82221324+Raselli@users.noreply.github.com>
2022-05-18 10:25:23 +0200

    replaced ArrayFields with ManytoManyFields in models.py

--- network/models.py
+++ network/models.py
@@ -1,26 +1,24 @@
 from django.contrib.auth.models import AbstractUser
-from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
 class User(AbstractUser):
     pass
 
+class Profile(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    following = models.ManyToManyField("self", blank=True)
+    followers = models.ManyToManyField("self", blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username} follows {self.following} person(s) and has {self.followers} follower(s)" 
+
 class Post(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
     content = models.CharField(max_length=512)
-    likes = models.IntegerField(default=0)
     posted = models.DateTimeField(auto_now_add=True)
-     
+    likes = models.IntegerField(default=0)    
+    liked = models.ManyToManyField(Profile, related_name="profiles")
+    
     def __str__(self):
-        return f"{self.username} posted on {self.posted}: \"{self.content}\". [{self.likes} like(s)]" 
-
-class Profile(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    followers = models.IntegerField(default=0)
-    following = ArrayField(models.IntegerField(unique=True))
-    liked = ArrayField(models.IntegerField(unique=True))   
-    # ? either: query for assosication list 'followers' & 'following' or save int on profile
-
-    def __str__(self):
-        return f"{self.user.username} has {self.followers} follower(s) and follows {self.following}" 
+        return f"{self.user.username} posted on {self.posted}: {self.content}. {self.likes} like(s)." 
