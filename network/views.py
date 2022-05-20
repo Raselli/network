86976ebdcbdb6da5014787cd37fec8ab6208bd4c
Raselli <82221324+Raselli@users.noreply.github.com>
2022-05-20 11:24:48 +0200

    added following in views.py

--- network/views.py
+++ network/views.py
@@ -1,7 +1,7 @@
 from queue import Empty
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django import forms
@@ -52,6 +52,8 @@ def index(request):
         # display user's posts (reverse chronological order: newest -> oldest)
         # if visitor != user: follow/unfollow button
 
+# Load others profiles
+    # profile on seperate page or on same?  
 def profile(request, profile_name):
 
     # Query profile
@@ -62,6 +64,10 @@ def profile(request, profile_name):
     if request.method == "POST":
         current_users_profile = request.user.profile
         
+        # Check for request manipulation
+        if other_user.id == request.user.id:
+            return HttpResponseBadRequest('It is not possible to follow/unfollow yourself.')
+
         # Add/remove item from Profile.followings   
         if "follow" in request.POST:
             current_users_profile.following.add(other_users_profile)
@@ -78,36 +84,42 @@ def profile(request, profile_name):
         "profile": Profile.objects.prefetch_related().get(user=other_user.id),
         "posts": Post.objects.filter(user=other_user.id).select_related().order_by('-posted')
     })
-# profile on seperate page or on same?
 
 
-
-
+# Posts of people the User follows
+#TODO:
+    # ? merge with 'def posts' under same rendering with switchstatement/dict of different db queries ?
 @login_required(login_url='login')
 def following(request):
-    current_user_profile = request.user.profile        
-    
-#TODO:
-    # load all (recent?) posts of users the auth. user follows
-    # login req.
-    # ? merge with 'def posts' under same rendering with switchstatement/dict of different db queries ?
-    # Pagination:
-        # render only 10 postsat once
-        # next button for next 10 posts
-        # back button for prev 10 posts
- 
+    current_users_profile = request.user.profile
+    current_user_isfollowing = current_users_profile.following.all()
+    other_users_pks = []
+    for profile in current_user_isfollowing:
+        other_users_pk = profile.user.id
+        other_users_pks.append(other_users_pk) 
     return render(request, "network/index.html" , {
-        "follow": current_user_profile.following.all()
+        "posts": Post.objects.filter(pk__in=other_users_pks).select_related().order_by('-posted')
     })
 
-# ? add ModelForm edit_post ?
-# or use jinja/JS
-  
-# def edit_post
-    # load textarea with posts content inside
-    # save edited content with save button
-        # save with JS: no re-render of page
-    # check security: token, manipulation, get/post: check user_id
+
+
+
+#TODO:   
+# Pagination:
+    # render only 10 postsat once
+    # next button for next 10 posts
+    # back button for prev 10 posts
+
+# edit post
+    # modelForm
+        # ? add ModelForm edit_post ?
+        # or use jinja/JS
+    
+    # def edit_post
+        # load textarea with posts content inside
+        # save edited content with save button
+            # save with JS: no re-render of page
+        # check security: token, manipulation, get/post: check user_id
 
 
 
